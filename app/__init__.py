87b8b8d2ab062e01725a6872ad4a244afa1bfd16
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2022-09-27 17:09:09 -0500

    first push4

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
+from flask import Flask
+
+from app.config import Config
+
+server = Flask(__name__)
+server.config.from_object(Config)
 
 
-from redis import Redis
-from rq import Queue
-from dblistener import DBListener
-import os
-
-q = Queue(connection=Redis(host='redis', port=6379, decode_responses=True),default_timeout=-1)
-result = q.enqueue(DBListener(os.environ.get('psycopg_url'), os.environ.get('psycopg_db'), os.environ.get('psycopg_port'), os.environ['dbUserName'],os.environ['dbPassword']).dblisten)
