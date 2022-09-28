750a7b8565240b014539a0b0da783a790cb8a5ca
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2022-09-28 15:46:22 -0500

    first push20

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from app import views
+
 
 from app.config import Config
 
 server = Flask(__name__)
-server.config.from_object(Config)#
+server.config.from_object(Config)
+
+from app import views
 
 
