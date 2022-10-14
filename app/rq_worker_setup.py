911dfd882cc2b340d4de72fb6d40c91d9e864cd9
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2022-10-14 17:14:25 -0500

    import queries

--- app/rq_worker_setup.py
+++ app/rq_worker_setup.py
@@ -3,6 +3,8 @@ from rq import Queue
 from dblistener import DBListener#
 import os
 from app import db, metadata
+from sqlalchemy.sql import text
+from queries import student_table_trigger_text,tutor_table_trigger_text,lead_table_trigger_text,prospect_table_trigger_text
 
 q = Queue(connection=Redis(host='redis', port=6379, decode_responses=True,password=os.environ.get('REDIS_PASSWORD')),default_timeout=-1)
 result = q.enqueue(DBListener(os.environ.get('psycopg_url'), os.environ.get('psycopg_db'), os.environ.get('psycopg_port'), os.environ.get('dbUserName'),os.environ.get('dbPassword')).dblisten)
