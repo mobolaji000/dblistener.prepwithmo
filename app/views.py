befbb66fc9eb83af25eae9f78730a03c5eed2d66
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2022-10-20 14:50:33 -0500

    trace directories48

--- app/views.py
+++ app/views.py
@@ -9,15 +9,16 @@ colorama.init(strip=False)
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
 stream_handler = logging.StreamHandler()
 file_handler = logging.FileHandler(str('logs/')+str(os.path.basename(__file__)[:-3])+'.log','a')
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
-
-logger.debug("testing logger")
 
 from app import server
 from app.aws import AWSInstance
