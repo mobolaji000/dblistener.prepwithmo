ccbb8d7b3cd63496c461a6726b220039807ca4bf
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2022-10-21 16:45:35 -0500

    trace directories70

--- app/views.py
+++ app/views.py
@@ -14,7 +14,7 @@ file_handler = logging.FileHandler(str('logs/')+'logs'+'-'+str(datetime.datetime
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 stream_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)#
 
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
