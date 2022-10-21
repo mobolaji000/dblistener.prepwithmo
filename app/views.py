ed796d62d4a7d4425d4e27953c8e5f41f9403cad
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2022-10-21 16:52:42 -0500

    trace directories71

--- app/views.py
+++ app/views.py
@@ -5,16 +5,17 @@ colorama.init(strip=False)
 
 import logging
 import datetime
+import pytz
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 stream_handler = logging.StreamHandler()
-file_handler = logging.FileHandler(str('logs/')+'logs'+'-'+str(datetime.datetime.now().strftime('%Y-%m-%d---%H-%M'))+'.log','a')
+file_handler = logging.FileHandler(str('logs/')+'logs'+'-'+str(pytz.timezone('US/Central').localize(datetime.datetime.now()).strftime('%Y-%m-%d---%H-%M'))+'.log','a')
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 stream_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)#
+file_handler.setFormatter(formatter)
 
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
