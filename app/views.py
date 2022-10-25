93f6b4b97842d224ef780f238fe1f3cb8d3e30b1
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2022-10-25 14:12:08 -0500

    fix pg11

--- app/views.py
+++ app/views.py
@@ -39,8 +39,8 @@ def hello():
 
     RQWorkerSetup()
     logger.debug("You have landed on the main route")
-    return (str(dd))
-    #return ("You have landed on the main route")
+    #return (str(dd))
+    return ("You have landed on the main route")
 
 @server.route("/health")
 def health():
