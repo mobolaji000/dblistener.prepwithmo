4ad830767f64250da2536ab78862e8718632a99a
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2022-10-27 22:00:06 -0500

    testing logtail60

--- app/rq_worker_setup.py
+++ app/rq_worker_setup.py
@@ -25,7 +25,7 @@ logger.addHandler(file_handler)
 
 class RQWorkerSetup():
     def __init__(self):
-        q = Queue(connection=Redis(host='redis', port=6379, decode_responses=True,password=os.environ.get('REDIS_PASSWORD')),default_timeout=-1)
+        q = Queue(connection=Redis(host='redis', port=6379, decode_responses=True,password=os.environ.get('REDIS_PASSWORD'),health_check_interval=10,socket_connect_timeout=5,retry_on_timeout=True,socket_keepalive=True),default_timeout=-1)
         result = q.enqueue(DBListener(os.environ.get('psycopg_url'), os.environ.get('psycopg_db'), os.environ.get('psycopg_port'), os.environ.get('DBUSERNAME'),os.environ.get('DBPASSWORD')).dblisten)
 
         try:
